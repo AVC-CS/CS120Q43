914249aa40585992543555b80996e232ac1d5bf5
Kyuwoong Lee <kwooler@gmail.com>
2023-09-21 17:55:50 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,21 +4,8 @@ using namespace std;
 
 int main()
 {
-    const double rate1 = 1.10;
-    const double rate2 = 2.20;
-    const double rate3 = 3.70;
-    const double rate4 = 4.80;
-    double total_charge;
-    double weight, distance, rate;
-
-    cout << "Enter the package weight and distance: ";
-    cin >> weight >> distance;
-
-    /*
-    Code you program here
-    */
-
+    int max, min, med;
+    
     // Use the followig print statements
-    cout << setw(10) << left << setprecision(2) << fixed;
-    cout << total_charge << endl;
+    cout << max << "\t" << med << "\t" << min << endl;
 }
